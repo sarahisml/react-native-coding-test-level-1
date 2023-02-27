9aa736b58cc8cf56c6338da539187ac4a16ffddb
sarahisml <sarah.isml22@gmail.com>
2023-02-27 17:00:47 +0800

    Finish catalog

--- App.js
+++ App.js
@@ -1,23 +1,83 @@
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import Home from "./src/screens/Home";
-import Form from "./src/screens/Form";
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useCallback, useEffect } from "react";
+import { StyleSheet, Text, View, FlatList, Image, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-const Stack = createStackNavigator();
 
-function App() {
+const pokePath = "https://pokeapi.co/api/v2/";
+const pokeQuery = "pokemon?limit=10&offset=0";
+const firstGenPokemonPath = `${pokePath}${pokeQuery}`;
+
+export default function App() {
+  const [firstGenPokemonDetails, setfirstGenPokemonDetails] = useState([]);
+
+  useEffect(() => {
+    const fetchFirstGenPokemons = async () => {
+      const firstGenPokemonIdsResponse = await fetch(firstGenPokemonPath);
+      const firstGenPokemonIdsBody = await firstGenPokemonIdsResponse.json();
+
+      const firstGenPokemonDetails = await Promise.all(
+        firstGenPokemonIdsBody.results.map(async (p) => {
+          const pDetails = await fetch(p.url);
+
+          return await pDetails.json();
+        })
+      );
+
+      setfirstGenPokemonDetails(firstGenPokemonDetails);
+    };
+
+    fetchFirstGenPokemons();
+  }, []);
+
+  const renderPokemon = ({ item }) => {
+    return (
+      <View style={styles.pokemonContainer}>
+        <Button
+          title={item.name.charAt(0).toUpperCase() + item.name.slice(1)}
+        ></Button>
+        <Text style={styles.pokemonTitle}>
+          {item.name.charAt(0).toUpperCase() + item.name.slice(1)}
+        </Text>
+        <Image
+          style={styles.pokemonSprite}
+          source={{
+            uri: item.sprites.front_default,
+          }}
+        />
+      </View>
+    );
+  };
+
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Form" component={Form} />
-    </Stack.Navigator>
+    <View style={styles.container}>
+      <Text style={styles.title}>First Gen Pokemons</Text>
+      <FlatList data={firstGenPokemonDetails} renderItem={renderPokemon} />
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-export default () => {
-  return (
-    <NavigationContainer>
-      <App />
-    </NavigationContainer>
-  );
-};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    marginTop: 60,
+  },
+  title: {
+    fontSize: 38,
+    alignSelf: "center",
+    marginBottom: 20,
+  },
+  pokemonContainer: { backgroundColor: "lightgrey", marginTop: 10 },
+  pokemonTitle: {
+    fontSize: 32,
+    alignSelf: "center",
+    marginTop: 10,
+  },
+  pokemonSprite: {
+    width: 200,
+    height: 200,
+    alignSelf: "center",
+  },
+});
